33d0237d64827d03c44aab03764fb8ce93235349
Ted Brownlow <ted.brownlow@gmail.com>
2020-03-21 20:19:26 -0400

    only normalize to avoid clipping

--- app/src/main/java/com/tedb/looper/audio/AudioTrackHelper.kt
+++ app/src/main/java/com/tedb/looper/audio/AudioTrackHelper.kt
@@ -9,15 +9,25 @@ fun mixRecordings(currentRecording: AudioRecording?, newRecording: AudioRecordin
     )
     mixRecording.frameCount = currentRecording.frameCount
     mixRecording.weight = currentRecording.weight + 1
+    // mixing stage
+    var maxSample = 1
     for (i in 0 until currentRecording.frameCount) {
         val currentSample = currentRecording.buffer[i]
         //val newIndex = (i-newRecording.offset+newRecording.frameCount)%newRecording.frameCount
         val newIndex = i
         val newSample = newRecording.buffer[newIndex]
         val totalWeight = currentRecording.weight + newRecording.weight
-        val mixSample =
-            (currentSample*currentRecording.weight+newSample*newRecording.weight)/totalWeight
+        val mixSample = (currentSample+newSample)/2
+        if (mixSample>maxSample) maxSample = mixSample
         mixRecording.buffer[i] = mixSample.toShort()
+    }
+    // normalizing stage
+    val multFactor = Short.MAX_VALUE/maxSample
+    // only scale down
+    if (multFactor<1) {
+        for (i in 0 until mixRecording.frameCount) {
+            mixRecording.buffer[i] = (mixRecording.buffer[i] * multFactor).toShort()
+        }
     }
     return mixRecording
 }
